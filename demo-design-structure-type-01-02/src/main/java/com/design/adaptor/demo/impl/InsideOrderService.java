4b78172b8e7cc4dc702e2f66841133947a241c4b
maskchair <1285816859@qq.com>
2021-03-03 14:53:44 +0800

    设计模式之适配器模式

--- demo-design-structure-type-01-02/src/main/java/com/design/adaptor/demo/impl/InsideOrderService.java
+++ demo-design-structure-type-01-02/src/main/java/com/design/adaptor/demo/impl/InsideOrderService.java
@@ -1,13 +1,13 @@
 package com.design.adaptor.demo.impl;
 
-import com.design.adaptor.demo.OrderAdaptorService;
+import com.design.adaptor.demo.OrderAdapterService;
 import com.design.adaptor.demo.service.OrderService;
 
 /**
  * @author qinhuajiao
  * @date 2021年03月03日 10:50:54
  */
-public class InsideOrderService implements OrderAdaptorService {
+public class InsideOrderService implements OrderAdapterService {
 
     private OrderService orderService = new OrderService();
 
--- demo-design-structure-type-01-02/src/main/java/com/design/adaptor/demo/impl/POPOrderAdapterServiceImpl.java
+++ demo-design-structure-type-01-02/src/main/java/com/design/adaptor/demo/impl/POPOrderAdapterServiceImpl.java
@@ -1,13 +1,13 @@
 package com.design.adaptor.demo.impl;
 
-import com.design.adaptor.demo.OrderAdaptorService;
+import com.design.adaptor.demo.OrderAdapterService;
 import com.design.adaptor.demo.service.POPOrderService;
 
 /**
  * @author qinhuajiao
  * @date 2021年03月03日 10:53:18
  */
-public class POPOrderAdaptorServiceImpl implements OrderAdaptorService {
+public class POPOrderAdapterServiceImpl implements OrderAdapterService {
 
     private POPOrderService popOrderService = new POPOrderService();
 
