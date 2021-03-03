74c5a5fcaf386e0dc8f24ceb2e3d867adc78d278
maskchair <1285816859@qq.com>
2021-03-03 14:59:33 +0800

    Revert "设计模式之适配器模式" 修改类名 This reverts commit 4fcf42d0

--- demo-design-structure-type-01-02/src/test/java/com/design/adaptor/demo/test/ApiTest.java
+++ demo-design-structure-type-01-02/src/test/java/com/design/adaptor/demo/test/ApiTest.java
@@ -1,11 +1,11 @@
 package com.design.adaptor.demo.test;
 
 import com.alibaba.fastjson.JSON;
-import com.design.adaptor.demo.MQAdapter;
-import com.design.adaptor.demo.OrderAdapterService;
+import com.design.adaptor.demo.MQAdaptor;
+import com.design.adaptor.demo.OrderAdaptorService;
 import com.design.adaptor.demo.RebateInfo;
 import com.design.adaptor.demo.impl.InsideOrderService;
-import com.design.adaptor.demo.impl.POPOrderAdapterServiceImpl;
+import com.design.adaptor.demo.impl.POPOrderAdaptorServiceImpl;
 import com.design.adaptor.demo.mq.CreateAccount;
 import com.design.adaptor.demo.mq.OrderMq;
 import org.junit.Test;
@@ -38,7 +38,7 @@ public class ApiTest {
         link01.put("bizId", "number");
         link01.put("bizTime","accountDate");
         link01.put("desc", "desc");
-        RebateInfo rebateInfo01 = MQAdapter.filter(createAccount.toString(), link01);
+        RebateInfo rebateInfo01 = MQAdaptor.filter(createAccount.toString(), link01);
 
         System.out.println("mq.create_account(适配前)" + createAccount.toString());
         System.out.println("mq.create_account(适配后)" + JSON.toJSONString(rebateInfo01));
@@ -55,17 +55,17 @@ public class ApiTest {
         link02.put("userId", "uid");
         link02.put("bizId", "orderId");
         link02.put("bizTime", "createOrderTime");
-        RebateInfo rebateInfo02 = MQAdapter.filter(orderMq.toString(), link02);
+        RebateInfo rebateInfo02 = MQAdaptor.filter(orderMq.toString(), link02);
         System.out.println("mq.orderMq(适配前)" + orderMq.toString());
         System.out.println("mq.orderMq(适配后)" + JSON.toJSONString(rebateInfo02));
     }
 
     @Test
     public void test_itfAdapter() {
-        OrderAdapterService popOrderAdapterService = new POPOrderAdapterServiceImpl();
+        OrderAdaptorService popOrderAdapterService = new POPOrderAdaptorServiceImpl();
         System.out.println("判断首单，接口适配(POP)：" + popOrderAdapterService.isFirst("10020001"));
 
-        OrderAdapterService insideOrderService = new InsideOrderService();
+        OrderAdaptorService insideOrderService = new InsideOrderService();
         System.out.println("判断首单，接口适配(自营)：" + insideOrderService.isFirst("10020001"));
     }
 }
